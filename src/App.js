c13a43a9aea6ada0a89b4a6cff41ba7416c953d8
catev98 <vicente.bcatarina@gmail.com>
2022-08-14 13:46:43 +0100

    framework

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
+      <div className="container">
+      </div>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">Coded by Catarina Vicente</footer>
     </div>
   );
 }
-
-
