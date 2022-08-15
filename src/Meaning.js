83eb9379e1cadab0147d3997d76e90498c80c7b4
catev98 <vicente.bcatarina@gmail.com>
2022-08-15 10:53:06 +0100

    changes

--- src/Meaning.js
+++ src/Meaning.js
@@ -10,12 +10,10 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              <strong>Definition:</strong>
-              {definition.definition}
+              <strong>Definition:</strong> {definition.definition}
               <br />
               <strong>Example:</strong>
               <em>{definition.example}</em>
-              <br />
               <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
