53516e14427703236f9d1e75c5e7beede12318c4
catev98 <vicente.bcatarina@gmail.com>
2022-08-15 11:14:42 +0100

    css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -5,11 +6,16 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary defaultKeyword="sunset" />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by SheCodes</small>
+        </footer>
       </div>
-      <main>
-        <Dictionary />
-      </main>
-      <footer className="App-footer">Coded by Catarina Vicente</footer>
     </div>
   );
 }
